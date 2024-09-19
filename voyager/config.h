8dbf6043419e9a45e87a41d6f97f6544633546d5
Scott Stevenson <scott@stevenson.io>
2024-09-19 11:41:05 +0100

    Reduce tapping term to 250 ms

--- voyager/config.h
+++ voyager/config.h
@@ -3,14 +3,11 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 
-#define ORYX_CONFIGURATOR
 #undef TAPPING_TERM
-#define TAPPING_TERM 300
+#define TAPPING_TERM 250
 
 #define USB_SUSPEND_WAKEUP_DELAY 0
-#define FIRMWARE_VERSION u8"lqWWM/lmQED"
-#define RAW_USAGE_PAGE 0xFF60
-#define RAW_USAGE_ID 0x61
+#define SERIAL_NUMBER "lqWWM/RDL3l"
 #define LAYER_STATE_8BIT
 
 #define RGB_MATRIX_STARTUP_SPD 60
