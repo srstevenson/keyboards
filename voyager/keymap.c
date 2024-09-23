73d57b419705e05a0b2d3eda6dc8e1c01150ee33
Scott Stevenson <scott@stevenson.io>
2024-09-23 22:33:14 +0100

    Swap back layer 1 and 2 toggles

--- voyager/keymap.c
+++ voyager/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,
     KC_LEFT_CTRL,   KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        MT(MOD_RCTL, KC_QUOTE),
     KC_LEFT_SHIFT,  MT(MOD_LALT, KC_Z),KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         MT(MOD_RALT, KC_SLASH),KC_RIGHT_SHIFT,
-                                                    MT(MOD_LGUI, KC_ENTER),LT(2,KC_ESCAPE),                                LT(1,KC_BSPC),  MT(MOD_RGUI, KC_SPACE)
+                                                    MT(MOD_LGUI, KC_ENTER),LT(1,KC_ESCAPE),                                LT(2,KC_BSPC),  MT(MOD_RGUI, KC_SPACE)
   ),
   [1] = LAYOUT_voyager(
     KC_ESCAPE,      KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,                                          KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
