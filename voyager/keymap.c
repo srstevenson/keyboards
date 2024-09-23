00fdd93068f1222cdb2803c1ef37a1c14a3de6eb
Scott Stevenson <scott@stevenson.io>
2024-09-23 22:23:12 +0100

    Swap inner and outer thumb keys

--- voyager/keymap.c
+++ voyager/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,
     KC_LEFT_CTRL,   KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_QUOTE,
     KC_LEFT_SHIFT,  MT(MOD_LALT, KC_Z),KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         MT(MOD_RALT, KC_SLASH),MT(MOD_RSFT, KC_BSPC),
-                                                    MO(2),          MT(MOD_LGUI, KC_ENTER),                                MT(MOD_RGUI, KC_SPACE),MO(1)
+                                                    MT(MOD_LGUI, KC_ENTER),MO(2),                                          MO(1),          MT(MOD_RGUI, KC_SPACE)
   ),
   [1] = LAYOUT_voyager(
     KC_ESCAPE,      KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,                                          KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
